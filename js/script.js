f9e2205b174b1ef533b91f9157e8e412a398312f
lulumoha <lulumoha123@gmail.com>
2017-12-03 11:54:56 +0300

    starting on the business logic

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//business logic :contructors and prototype
+var player1="";
+var player2="";
+
+var throwdice = function () {
+  return Math.floor(Math.random()*6)+1; //math.random () returns a pseudo-random number between 0-1
+}                                       //The Math.floor() function returns the largest integer less than or equal to a given number.
