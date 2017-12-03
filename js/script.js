043d7b75d19c831214f87598c908afe151eae629
lulumoha <lulumoha123@gmail.com>
2017-12-03 12:11:36 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -90,3 +90,13 @@ $(document).ready(function() {
        player2.rollone();
        $("#round-total-2").text(player2.score);
      });
+
+     $("button#player2-hold").click(function(event){
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+    $("#roll-2").empty();
+    player2.winnerCheck();
+  });
+
+});
