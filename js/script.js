8c9ff473a805541f0659f8dccfcb77d500186567
lulumoha <lulumoha123@gmail.com>
2017-12-03 11:59:10 +0300

    add player function and prototype

--- js/script.js
+++ js/script.js
@@ -4,4 +4,21 @@ var player2="";
 
 var throwdice = function () {
   return Math.floor(Math.random()*6)+1; //math.random () returns a pseudo-random number between 0-1
-}                                       //The Math.floor() function returns the largest integer less than or equal to a given number.
+}
+                                  //The Math.floor() function returns the largest integer less than or equal to a given number.
+function Player(turn) {
+  this.roll = 0;
+  this.score = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.score = 0;
+  alert("You rolled a 1!" + " "+ this.playerName)
+  } else {
+  this.score += this.roll;
+  }
+}
