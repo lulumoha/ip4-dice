4bd116a628fda4d34d3cfa3e494d2f61ecd0712e
lulumoha <lulumoha123@gmail.com>
2017-12-03 15:22:07 +0300

    update changes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ var player2="";
 
 var throwdice = function () {
   return Math.floor(Math.random()*6)+1; //math.random () returns a pseudo-random number between 0-1
-}
-                                  //The Math.floor() function returns the largest integer less than or equal to a given number.
+}                                       //The Math.floor() function returns the largest integer less than or equal to a given number.
+
 function Player(turn) {
   this.roll = 0;
   this.score = 0;
@@ -49,6 +49,8 @@ var clearValues = function(){
   $(".player2Name").val("");
 }
 
+
+// User Interface
 $(document).ready(function() {
 
   $("button#start").click(function(event){
@@ -66,32 +68,31 @@ $(document).ready(function() {
     player1.playerName=player1Name;
     player2.playerName=player2Name;
 
-     });
+  });
+  //roll dice
+  $("button#player1-roll").click(function(event){
+    player1.roll = throwdice();
+    $("#roll-1").text(player1.roll);
+    player1.rollone();
+    $("#round-total-1").text(player1.score);
+  });
 
-     //roll dice
-     $("button#player1-roll").click(function(event){
-       player1.roll = throwdice();
-       $("#roll-1").text(player1.roll);
-       player1.rollone();
-       $("#round-total-1").text(player1.score);
-     });
+  $("button#player1-hold").click(function(event){
+    player1.hold();
+    $("#total-score-1").text(player1.totalscore);
+    $("#round-total-1").empty();
+    $("#roll-1").empty();
+    player1.winnerCheck();
+  });
 
-     $("button#player1-hold").click(function(event){
-       player1.hold();
-       $("#total-score-1").text(player1.totalscore);
-       $("#round-total-1").empty();
-       $("#roll-1").empty();
-       player1.winnerCheck();
-     });
+  $("button#player2-roll").click(function(event){
+    player2.roll = throwdice();
+    $("#roll-2").text(player2.roll);
+    player2.rollone();
+    $("#round-total-2").text(player2.score);
+  });
 
-     $("button#player2-roll").click(function(event){
-       player2.roll = throwdice();
-       $("#roll-2").text(player2.roll);
-       player2.rollone();
-       $("#round-total-2").text(player2.score);
-     });
-
-     $("button#player2-hold").click(function(event){
+  $("button#player2-hold").click(function(event){
     player2.hold();
     $("#total-score-2").text(player2.totalscore);
     $("#round-total-2").empty();
